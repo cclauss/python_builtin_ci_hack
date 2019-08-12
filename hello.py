088d06433da0aa82bf8c5b289d2a9e8c70b58691
Christian Clauss <cclauss@me.com>
2019-08-12 15:24:10 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
-def test_hello_world():  # placate pytest
-    pass
+def hello(who="world"):
+    return "Hello {}!".format(who)
+
+
+def test_hello():
+    assert hello() == "Hello world!"
+    assert hello("test") == "Hello test!"    
 
 
 if __name__ == "__main__":
-    print("Hello world!")
+    print()
